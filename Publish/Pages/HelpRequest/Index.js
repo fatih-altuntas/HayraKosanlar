331e34b5cc810209f838b071fbc4b1fe3be9d00a
baris <barisguzellik@gmail.com>
2020-11-18 17:39:34 +0300

    Publish

---
+++ Publish/Pages/HelpRequest/Index.js
@@ -0,0 +1,103 @@
+﻿$(function () {
+    $('#HelpRequest_Status').css('pointer-events', 'none');
+    $('#HelpRequest_Status').parent().css('cursor', 'no-drop');
+
+    var l = abp.localization.getResource('HayraKosanlar');
+    var createModal = new abp.ModalManager(abp.appPath + 'HelpRequest/CreateModal');
+    var editModal = new abp.ModalManager(abp.appPath + 'HelpRequest/EditModal');
+    var status = 1;
+
+    var dataTable = $('#HelpRequestsTable').DataTable(
+        abp.libs.datatables.normalizeConfiguration({
+            orderCellsTop: true,
+            fixedHeader: true,
+            paging: true,
+            order: [[1, "asc"]],
+            searching: true,
+            scrollX: true,
+            ajax: abp.libs.datatables.createAjax(hayraKosanlar.helpRequests.helpRequest.listByStatus, function () {
+                return status;
+            }),
+            columnDefs: [
+                {
+                    title: l('Name'),
+                    data: "name",
+                    searchable: "true"
+                },
+                {
+                    title: l('Surname'),
+                    data: "surname"
+                },
+                {
+                    title: l('IdentityNumber'),
+                    data: "identityNumber"
+                },
+                {
+                    title: l('PhoneNumber'),
+                    data: "phoneNumber"
+                },
+                {
+                    title: l('ExtraInformation'),
+                    data: "extraInformation"
+                },
+                {
+                    title: l('Status'),
+                    data: "status",
+                    render: function (data) {
+                        return l('Enum:HelpRequestStatus:' + data);
+                    }
+                },
+                {
+                    title: l('CreationTime'), data: "creationTime",
+                    render: function (data) {
+                        return luxon
+                            .DateTime
+                            .fromISO(data, {
+                                locale: abp.localization.currentCulture.name
+                            }).toLocaleString(luxon.DateTime.DATETIME_SHORT);
+                    }
+                },
+                {
+                    title: l('Actions'),
+                    rowAction: {
+                        items:
+                            [
+                                {
+                                    text: l('Edit'),
+                                    visible: abp.auth.isGranted('HayraKosanlar.EditButton'),
+                                    action: function (data) {
+                                        editModal.open({ id: data.record.id });
+                                    }
+                                },
+                                {
+                                    text: l('View'),
+                                    visible: abp.auth.isGranted('HayraKosanlar.ViewButton'),
+                                    action: function (data) {
+                                        editModal.open({ id: data.record.id });
+                                    }
+                                }
+                            ]
+                    }
+                }
+            ]
+        })
+    );
+    $('#NewHelpRequestButton').click(function (e) {
+        e.preventDefault();
+        createModal.open();
+    })
+    editModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+    createModal.onResult(function () {
+        dataTable.ajax.reload();
+    })
+
+    $('select').on('change', function () {
+        status = this.value;
+        dataTable.ajax.reload();
+    });
+    if (!abp.auth.isGranted('HayraKosanlar.Create')) {
+        $('#status-dropdown').css("display", "none");
+    }
+});
---
+++ Publish/Pages/Index.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    var createModal = new abp.ModalManager(abp.appPath + 'HelpRequest/CreateModal');
+    var createHandModal = new abp.ModalManager(abp.appPath + 'GiveAHandRequest/CreateModal');
+    abp.log.debug('Index.js initialized!');
+    $('#NewHelpRequestButton').click(function (e) {
+        e.preventDefault();
+        createModal.open();
+    })
+    $('#NewGiveAHandRequestButton').click(function (e) {
+        e.preventDefault();
+        createHandModal.open();
+    });
+    createModal.onResult(function () {
+        abp.notify.success("Yardım talebiniz oluşturuldu.")
+    });
+    createHandModal.onResult(function () {
+        abp.notify.success("Yardım eli uzatma talebiniz oluşturuldu.")
+    });
+});
